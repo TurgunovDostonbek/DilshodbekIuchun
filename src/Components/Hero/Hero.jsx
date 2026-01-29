09db783eecb54ae3f4cddf9f55607b38b0527395
Turgunov Dostonbek <turgunovdostonbek@gmail.com>
2026-01-29 11:37:27 +0500

    DI

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,20 @@
 import './Hero.css'
+import Im from '../../assets/images/222.png'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='hero container'>
+      <div className="hero__left">
+        <h1 className='hero__left--title'>I'm Dilshodbek</h1>
+        <p className='hero__left--desc'>A passionate Frontend Developer</p>
+        <button className='hero__left--btn'>Hire Me</button>
+      </div>
+      <div className="hero__right">
+        <div className="hero__right--imgbox">
+          <img className='hero__right--img' src={Im} alt="Hero Image"  />  
+        </div>
+      </div>
+    </div>
   )
 }
 
