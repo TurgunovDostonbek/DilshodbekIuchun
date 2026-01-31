9e06b78323191576b82916860261e278a5e4c86d
Turgunov Dostonbek <turgunovdostonbek@gmail.com>
2026-01-31 11:34:26 +0500

    hhh

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import { useState } from "react";
+import Skills from './Components/Skills/Skills';
 function App() {
   let [barsOpen, setBarsOpen] = useState(false);
   return (
     <div>
       <Navbar barsOpen={barsOpen} setBarsOpen={setBarsOpen} />
       <Hero />
+      <Skills />
     </div>
   );
 }
---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,26 @@
+import "./Skills.css";
+
+function Skills() {
+  return (
+    <div className="skill container">
+      <h1>Skills</h1>
+      <div className="skill__cards">
+        <div className="my__skills">
+          <h1>Frontend Developer</h1>
+          <br />
+          <h2>HTML --- 100%</h2>
+          <h2>CSS --- 100%</h2>
+          <h2>JS --- 75%</h2>
+          <h2>REACT --- 30%</h2>
+        </div>
+        <div className="my__skills">
+          <h1>Backend Developer</h1>
+          <br />
+          <h1>NO.</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
