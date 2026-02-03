f80d58484c4f414764e008790a0ce5d0b60b99b9
Turgunov Dostonbek <turgunovdostonbek@gmail.com>
2026-02-03 11:27:04 +0500

    jrfjfrj

--- src/Components/Hobby/Hobby.jsx
+++ src/Components/Hobby/Hobby.jsx
@@ -1,30 +1,53 @@
-import "./Hobby.css"
-import hobbyimg from "../../assets/images/1765336962_uUNkm6w3if.webp"
-import hobbyimg1 from "../../assets/images/1-21-mcpe.webp"
+import "./Hobby.css";
+import hobbyimg from "../../assets/images/1765336962_uUNkm6w3if.webp";
+import hobbyimg1 from "../../assets/images/1-21-mcpe.webp";
 
 function Hobby() {
   return (
-    <div className='Hobby_Asosiysi container'>
+    <div className="Hobby_Asosiysi container">
       <div className="Hobby1">
         <h1>Hobby</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet vulputate tristique quam felis. Id phasellus dui orci vulputate consequat nulla proin. Id sit scelerisque neque, proin bibendum diam.</p>
+        <p>
+          🎮 O‘yin o‘ynashni yoqtirasiz (ayniqsa PUBG kabi), o‘yinlarda taktika
+          va kuchli o‘ynash sizga yoqadi. <br />
+           📚 O‘rganishga qiziqasiz — qonun,
+          huquqiy mavzularni so‘rab, bandma-band tushunib olasiz. <br /> 🗣️ Tilga ham
+          qiziqasiz — uzbekcha gaplarni to‘g‘ri aytish, tarjima va talaffuzni
+          ham tekshirtirib turasiz. <br /> 😂 Memlarni yoqtirasiz, ayniqsa italyancha
+          memlar ko‘p uchraydi sizda.
+        </p>
       </div>
 
       <div className="Hobby2">
-        <div className='Hobby2_text'>
-          <img src={hobbyimg} alt="" /><br />
+        <div className="Hobby2_text">
+          <img src={hobbyimg} alt="" />
+          <br />
           <h4>March 20, 2025 (PUBGm)</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, quis consectetur veritatis ullam odio, placeat laborum nostrum nobis enim quasi optio voluptate natus eligendi amet. Deserunt officiis aspernatur nisi reprehenderit.</p>
+          <p>
+            PUBG Mobile — bu ommabop battle royale janridagi mobil o‘yin.
+            O‘yinda 100 nafargacha o‘yinchi bitta xaritaga tushib, qurol-aslaha
+            topadi va oxirgi tirik qolgan jamoa yoki o‘yinchi g‘alaba qozonadi.
+            PUBG Mobile strategiya, tezkor reaksiya, aniqlik va jamoaviy
+            hamkorlikni talab qiladi. O‘yin Android va iOS platformalarida
+            mavjud bo‘lib, doimiy yangilanishlar va turli rejimlarga ega.
+          </p>
         </div>
 
-        <div className='Hobby2_text'>
+        <div className="Hobby2_text">
           <img src={hobbyimg1} alt="" />
           <h4>June 13, 2020 (MCPE)</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore sapiente fuga architecto mollitia cumque, esse enim quam incidunt soluta! Veniam accusamus nihil nulla quod similique voluptates animi neque veritatis suscipit?</p>
+          <p>
+            Minecraft: Pocket Edition (MCPE) — bu sandbox janridagi mashhur
+            mobil o‘yin. Unda o‘yinchilar bloklar yordamida qurish, resurs
+            yig‘ish, omon qolish (Survival) va erkin ijod qilish (Creative)
+            imkoniyatiga ega. MCPE ijodkorlik, mantiqiy fikrlash va tasavvurni
+            rivojlantiradi. O‘yin Android va iOS qurilmalarida o‘ynaladi hamda
+            multiplayer rejimini qo‘llab-quvvatlaydi.
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hobby
+export default Hobby;
